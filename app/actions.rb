df3bb8c0978251be95ab50009195c8fecf84459a
jaybach <dr.jaybach@gmail.com>
2015-09-21 20:40:52 +0000

    adds validation and error messages

--- app/actions.rb
+++ app/actions.rb
@@ -19,8 +19,11 @@ post '/songs' do
     author: params[:author],
     url: params[:url]
     )
-  @song.save
-  redirect'/songs'
+  if @song.save
+    redirect'/songs'
+  else
+    erb :'/songs/new'
+  end
 end
 
 get '/songs/:id' do
